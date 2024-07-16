a630d485facd3104a7e6a678ebe5c3bfc1bcec7d
Krishna Chandra Roy <roykishan707@gmail.com>
2024-07-17 00:55:53 +0530

    deloyment frontendleftover

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -5,7 +5,7 @@ export default function IndexPage() {
   const [posts, setPosts] = useState([]);
   
   useEffect(() => {
-    fetch('http://localhost:4000/post')
+    fetch('https://blog-application-shxy.onrender.com/post')
       .then(response => response.json())
       .then(posts => {
         setPosts(posts);
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -21,7 +21,7 @@ const LoginPage = (ev) => {
   const login=async (ev)=>{
     ev.preventDefault();
     
-    const response= await fetch('http://localhost:4000/login' ,{
+    const response= await fetch('https://blog-application-shxy.onrender.com/login' ,{
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers:{'Content-Type' : 'application/json'},
